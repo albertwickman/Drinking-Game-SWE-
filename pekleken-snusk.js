da9962437b3184136378a5bbf5a679ff3d72a15c
albertwickman <58918868+albertwickman@users.noreply.github.com>
2024-05-17 13:48:44 +0200

    New questions pekleken-snusk.js

--- pekleken-snusk.js
+++ pekleken-snusk.js
@@ -1,8 +1,8 @@
 const questions = [
-    "Mest benägen att ha haft en one night stand?",
+    "Är mest benägen att ha haft en one night stand?",
     "Skulle du vilja byta kropp med för en dag på grund av deras sex appeal?",
-    "Ansedd som bäst i sängen i detta rum?",
-    "Troligast att ha haft sex utomhus?",
+    "Är ansedd som bäst i sängen i detta rum?",
+    "Har troligast haft sex utomhus?",
     "Tros ha en hemlig samling sexleksaker?",
     "Skulle du vilja se i en sexig outfit?",
     "Tros ha haft flest sexpartners?",
@@ -11,7 +11,7 @@ const questions = [
     "Skulle du vilja ha en erotisk dröm om?",
     "Troligast att ha skickat nakenbilder?",
     "Skulle du välja för en het kyss?",
-    "Tros ha haft en \"friends with benefits\"?",
+    "Tros ha haft en 'KK'?",
     "Har den sexigaste rösten?",
     "Ansedd som mest flirtig på fester?",
     "Skulle troligen ha den mest intressanta erotiska dagboken?",
@@ -48,8 +48,34 @@ const questions = [
     "Tros vara den största romantikern?",
     "Skulle kunna hålla den längsta förspelsessionen?",
     "Skulle vara mest benägen att testa exotiska sexställningar?",
-    "Har den mest dristiga fantasin?"
+    "Har den mest dristiga fantasin?",
+    "Skulle mest troligt skicka ett pinsamt sms till fel person?",
+    "Är mest trolig att bli kär i sin bästa väns partner?",
+    "Skulle kunna kyssa en främling på en fest?",
+    "Skulle sannolikt prova på speed dating?",
+    "Är mest trolig att ha en hemlig admiratör?",
+    "Skulle mest troligt gå på en blinddejt arrangerad av en vän?",
+    "Skulle flirta med en polis för att komma undan en biljett?",
+    "Är mest trolig att bli 'ghostad' efter en dejt?",
+    "Skulle säga 'jag älskar dig' först?",
+    "Skulle mest troligt spela 'truth or dare' på en första dejt?",
+    "Skulle mest troligt ha en krånglig kärlekstriangel?",
+    "Är mest trolig att glömma namnet på personen de dejtar?",
+    "Skulle ha en hemlig crush på en kollega?",
+    "Skulle gå på flest Tinderdejter på en vecka?",
+    "Skulle bli upprörd om de fick reda på att deras ex dejtar någon ny?",
+    "Skulle mest troligt ha en online-romans?",
+    "Är mest trolig att bli kär på semestern?",
+    "Skulle kunna delta i ett reality-TV-program om dejting?",
+    "Skulle ha mest troligt en dejt som slutar med en natt i arresten?",
+    "Är mest trolig att skaffa en matchande tatuering med en partner?",
+    "Skulle mest troligt prova på partnerbyte på en fest?",
+    "Är mest trolig att prova på online dejting?",
+    "Skulle kunna bli förälskad i någons röst utan att ha sett personen?",
+    "Skulle mest troligt hålla handen på första dejten?",
+    "Skulle mest troligt ta med en vän på en dejt som backup?"
 ];
+```
 
 
 let shuffledQuestions = shuffleArray([...questions]);
